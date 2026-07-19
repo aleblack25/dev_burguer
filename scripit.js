7077ee04a5cd1acdcfb7a4340cdb2511a930f8b1
aleblack25 <alexandreacostacosta@gmail.com>
2026-07-18 22:48:06 -0300

    Estruturando o js.script e products.js

--- scripit.js
+++ scripit.js
@@ -1,2 +1,12 @@
 
+const list = document.querySelector('ul')
+
+list.innerHTML = `
+        <li>
+            <img src="./assets/bacon-egg.png" alt="">
+            <p>X-Bacon-Egg</p>
+            <p class="item-price">R$ 30,00</p>
+        </li>
+`
+
 
